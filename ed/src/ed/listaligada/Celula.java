458ad4659397966fdf1475490782b63b5f298a3b
Bruno Jacob <brunojacob_89@hotmail.com>
2022-06-18 16:44:10 -0300

    lista ligadas

---
+++ ed/src/ed/listaligada/Celula.java
@@ -0,0 +1,26 @@
+package ed.listaligada;
+
+public class Celula {
+	
+	private Object elemento;
+	private Celula proxima;
+	
+	public Celula(Object elemento, Celula proxima) {
+		super();
+		this.elemento = elemento;
+		this.proxima = proxima;
+	}
+	
+	public Celula getProxima() {
+		return proxima;
+	}
+	public void setProxima(Celula proxima) {
+		this.proxima = proxima;
+	}
+	public Object getElemento() {
+		return elemento;
+	}
+	
+	
+
+}
